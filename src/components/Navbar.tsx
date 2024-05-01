c40bc0c91f36fd4051d044d3a88ff537b4460683
loventnc <love.nattanischa2003@gmail.com>
2024-05-02 01:51:37 +0700

    Update_Navbar-username

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,32 @@
+"use client";
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import logoPath from "@/asset/img/Rectangle 60.svg";
 import Link from "next/link";
 import { Button } from "@mui/material";
+import { axiosInstance } from "@/lib/axiosInstance";
+import { useState, useEffect } from "react";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import { ClassNames } from "@emotion/react";
 
 const Navbar = () => {
+  const [user, setUser] = useState();
+
+  const fetchUserdata = async () => {
+    try {
+      const response = await axiosInstance.get("/api/user/getuserbyid");
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }; //ดึงข้อมูลผู้ใช้
+
+  useEffect(() => {
+    fetchUserdata();
+  }, [user]);
+
+  console.log(user);
   return (
     <Box className="bg-neutral01 flex items-center justify-between px-10 mx-10 my-5 rounded-xl h-14">
       <Box className="flex h-full items-center">
@@ -19,12 +40,27 @@ const Navbar = () => {
         <Link href="/predict">แบบทดสอบ</Link>
         <Link href={""}>ผลลัพธ์</Link>
         <Link href={"/community"}>ชุมชน</Link>
-        <Link href="/authenrize/login">
-          <Button variant="contained">เข้าสู่ระบบ</Button>
-        </Link>
-        <Link href="/authenrize/signup">
-          <Button variant="contained">สมัครใช้งาน</Button>
-        </Link>
+        {!user ? (
+          <>
+            <Link href="/authenrize/login">
+              <Button variant="contained">เข้าสู่ระบบ</Button>
+            </Link>
+            <Link href="/authenrize/signup">
+              <Button variant="contained">สมัครใช้งาน</Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Box className="flex items-center ">
+              <PersonOutlineOutlinedIcon className="mr-2" />
+              <Typography variant="body1">{user.username}</Typography>
+            </Box>
+            <Link href="/profile"></Link>
+            <Link href="/authenrize/logout">
+              <Button variant="text">ออกจากระบบ</Button>
+            </Link>
+          </>
+        )}
       </Box>
     </Box>
   );
