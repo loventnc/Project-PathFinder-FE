6dae718daff56b9346a2f0f6f7bb427bcf4a3610
loventnc <love.nattanischa2003@gmail.com>
2024-06-10 02:24:55 +0700

    Navbar and Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Grid, Box, Typography, Button } from "@mui/material";
-import Image from "next/image";
-import home from "@/asset/img/homepic.svg";
-import Link from "next/link";
 
-const HomePage: React.FC = () => {
+"use client";
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { Grid } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import home from "@/asset/img/homepic.svg";
+
+const HomePage = () => {
   return (
     <Box
       bgcolor="#FFFFFF"
@@ -13,38 +17,44 @@ const HomePage: React.FC = () => {
       justifyContent="center"
       alignItems="center"
       mx={5}
+      p={5}
     >
       <Grid container>
-        {/* Left*/}
+        {/* Left */}
         <Grid
-          display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          item
+          xs={12}
           md={6}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
         >
-          <Box textAlign="center">
+          <Box textAlign="center" className="p-5">
             <Typography
               variant="h1"
               color="primary"
-              className="font-bold font-noto"
+              className="text-[26px] sm:text-[40px] md:text-[60px] lg:text-[82px] font-bold font-noto"
             >
               PathFinder
             </Typography>
             <Typography
               variant="h5"
               color="primary"
-              className="font-bold font-noto"
+              className="text-[8px] sm:text-[12px] md:text-[16px] lg:text-[24px] font-bold font-noto"
             >
               “สร้างเส้นทางสู่อนาคตที่สำคัญของคุณ”
             </Typography>
             <Box
+              className="border-[1px] sm:border-[2px] md:border-[3px] lg:border-[4px]"
               textAlign="center"
-              borderBottom="5px solid #B4BFCC"
               width="100%"
               mt={2}
               mb={4}
             ></Box>
-            <Typography variant="body1" className="text-accent02 font-noto">
+            <Typography
+              variant="body1"
+              className="text-[8px] sm:text-[10px] md:text-[12px] lg:text-[16px] text-accent02 font-noto"
+            >
               ช่วยให้คุณสามารถสร้างเส้นทางสู่อนาคตที่แสนสำคัญของคุณได้อย่างมั่นใจ
               <br />
               เรามีแบบทดสอบที่สามารถทำให้คุณได้รับคำแนะนำที่น่าเชื่อถือในการเลือกอาชีพที่เหมาะสม
@@ -53,8 +63,10 @@ const HomePage: React.FC = () => {
               <Button
                 variant="contained"
                 size="large"
-                style={{ marginTop: 16 }}
-                className="font-bold font-noto bg-accent01 hover:bg-accent02"
+                className="font-bold font-noto bg-accent01 hover:bg-accent02 mt-4
+                 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl
+                 px-2 sm:px-4 md:px-6 lg:px-8 xl:px-9
+                 py-1 sm:py-2 md:py-3 lg:py-4 xl:py-5"
               >
                 เริ่มทำแบบทดสอบ
               </Button>
@@ -64,13 +76,13 @@ const HomePage: React.FC = () => {
 
         {/* Right */}
         <Grid
-          display={"flex"}
-          justifyContent={"center"}
-          direction={"column"}
+          item
+          xs={12}
           md={6}
-          width={"100%"}
-          paddingRight={"5%"}
-          gap={5}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          className="p-5"
         >
           <Box display="flex" justifyContent="center" alignItems="center">
             <Image src={home} alt="home" />
